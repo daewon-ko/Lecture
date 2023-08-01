f6078a0f9757e805951e7da041ee9fd2f5d978f0
pinetree <eodnjs568@gmail.com>
2023-08-02 08:44:46 +0900

    feat : 간단한 로깅관련 컨트롤러 작성 및 application.properties 통해 로그레벨 수준 설정

--- MVC1/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
+++ MVC1/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,13 +1,15 @@
 package hello.springmvc.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j  // 롬복 제공 어노테이션.해당 어노테이션 작성시 logger 클래스 import 필요없이 로깅 확인 가능
 public class LogTestController {
-    private final Logger log = LoggerFactory.getLogger(getClass());
+//    private final Logger log = LoggerFactory.getLogger(LogTestController.class);
 
     @RequestMapping("/log-test")
     public String logTest() {
