b52b213db2817a890368454b9d96e8cfb73705f2
pinetree <eodnjs568@gmail.com>
2023-08-02 09:17:08 +0900

    feat : 요청 Mapping 조건나열

--- MVC1/springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ MVC1/springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -1,15 +1,61 @@
 package hello.springmvc.basic.requestmapping;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
 public class MappingController {
-    @RequestMapping("/hello-basic")
+    @RequestMapping(value = "/hello-basic", method = RequestMethod.GET)
     public String helloBasic() {
         log.info("helloBasic");
+        return "ok";
+    }
+
+    /**
+     * PathVaialbe 사용
+     * 변수명이 같으면 생략
+     *
+     * @return
+     */
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId: {}", data);
+        return "ok";
+    }
+
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
+        log.info("mappingPath userId= {}, orderId = {} ", userId, orderId);
+
+        return "ok";
+    }
+
+    /*
+    잘 사용하지 않음.
+     */
+    @GetMapping(value = "/mapping-param", params = "mode!=debug")
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-header", headers = "mode=debug")
+    public String mappingHeader() {
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-consume", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    @GetMapping (value = "/mapping-produce", produces = "text/html")
+    public String mappingProduces() {
+        log.info("mappingProduces");
         return "ok";
     }
 }
