b04d35b55e5ac75775924539b6ae50abb628c679
pinetree <eodnjs568@gmail.com>
2024-01-09 20:03:50 +0900

    feat : Domain 분석 설계(1/2)
    - Domain 객체 등 구현

--- Spring/JPA1/src/main/java/jpa/shop/domain/Member.java
+++ Spring/JPA1/src/main/java/jpa/shop/domain/Member.java
@@ -1,5 +1,29 @@
 package jpa.shop.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+
 
 }
---
+++ Spring/JPA1/src/test/java/jpa/shop/repository/MemberRepositoryTest.java
@@ -0,0 +1,31 @@
+package jpa.shop.repository;
+
+import jpa.shop.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+//    @Rollback(value = false)
+    public void testMember() {
+        Member member = new Member();
+        Long savedId = memberRepository.save(member);
+
+        Member findMember = memberRepository.find(savedId);
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+}
