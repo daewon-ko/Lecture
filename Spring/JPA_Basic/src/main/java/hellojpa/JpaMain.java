66724b938d7aac608a2bfd6792136903cd0c7bfa
pinetree <eodnjs568@gmail.com>
2024-01-29 09:40:05 +0900

    chore : JPA 기본편 학습 위한 폴더 추가

---
+++ Spring/JPA_Basic/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,28 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        //code
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            Member member = new Member();
+            member.setId("ID_A");
+            member.setUsername("C");
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
