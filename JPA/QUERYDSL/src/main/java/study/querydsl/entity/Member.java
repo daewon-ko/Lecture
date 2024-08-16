fbff00920f286b3bbfdecf082ce76291e6b742f0
pinetree <eodnjs568@gmail.com>
2024-08-16 20:43:01 +0900

    QueryDSL 프로젝트 설정 및 섹션1

---
+++ JPA/QUERYDSL/src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,41 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        this.team = team;
+    }
+
+    public Member(String username) {
+        this(username, 0);
+    }
+
+    public Member(String username, int age) {
+      this(username, age, null);
+    }
+
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
