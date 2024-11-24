693775ce66cc36cf78e934242488ff778fd049a9
pinetree <eodnjs568@gmail.com>
2024-11-24 16:44:04 +0900

    spring batch study

---
+++ Spring/spring-batch/src/main/java/io/springbatch/springbatch/repository/WinRepository.java
@@ -0,0 +1,11 @@
+package io.springbatch.springbatch.repository;
+
+import io.springbatch.springbatch.entity.WinEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WinRepository extends JpaRepository<WinEntity, Long> {
+
+    Page<WinEntity> findByWinGreaterThanEqual(Long win, Pageable pageable);
+}
