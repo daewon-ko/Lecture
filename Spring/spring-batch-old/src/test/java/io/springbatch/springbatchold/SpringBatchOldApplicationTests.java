99cb542acc8186e04c030a9bd89a7ecf1cecdf01
pinetree <eodnjs568@gmail.com>
2024-12-30 09:12:47 +0900

    인프런 스프링 배치 섹션 3 DB 스키마 생성까지

---
+++ Spring/spring-batch-old/src/test/java/io/springbatch/springbatchold/SpringBatchOldApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbatch.springbatchold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchOldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
