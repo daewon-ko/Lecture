6826a0639e019ba5695307f877464eb98479ce90
pinetree <eodnjs568@gmail.com>
2025-01-11 21:07:31 +0900

    Spring Batch 강의

--- Spring/spring-batch-old/src/main/java/io/springbatch/springbatchold/StepConfiguration.java
+++ Spring/spring-batch-old/src/main/java/io/springbatch/springbatchold/StepConfiguration.java
@@ -2,56 +2,66 @@ package io.springbatch.springbatchold;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
-public class JobConfiguration {
+public class StepConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final ExecutionContextTasklet1 executionContextTasklet1;
+    private final ExecutionContextTasklet2 executionContextTasklet2;
+    private final ExecutionContextTasklet3 executionContextTasklet3;
+    private final ExecutionContextTasklet4 executionContextTasklet4;
+    private final JobExecutionListener jobExecutionListener;
+
+
 
     @Bean
     public Job job() {
         return jobBuilderFactory.get("job")
                 .start(step1())
                 .next(step2())
+                .next((step3()))
+                .next(step4())
+                .listener(jobExecutionListener)
                 .build();
     }
+
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet((contribution, chunkContext) -> {
-                    JobParameters jobParameters = contribution.getStepExecution().getJobExecution().getJobParameters();
-                    jobParameters.getString("name");
-                    jobParameters.getLong("seq");
-                    jobParameters.getDate("date");
-                    jobParameters.getDouble("age");
-
-
-                    Map<String, Object> jobParameters1 = chunkContext.getStepContext().getJobParameters();
-                    System.out.println("step1 was executed");
-                    return RepeatStatus.FINISHED;
-                })
+                .tasklet(executionContextTasklet1)
                 .build();
-
     }
 
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step2 was executed");
-                    return RepeatStatus.FINISHED;
-                })
+                .tasklet(executionContextTasklet2)
                 .build();
     }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet(executionContextTasklet3)
+                .build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet(executionContextTasklet4)
+                .build();
+    }
+
+
 }
